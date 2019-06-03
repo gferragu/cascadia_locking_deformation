6373d924b5859d24c291913086a01b9c7e113681
Alexis Klimasewski <aklimase@d92-234.uoregon.edu>
2019-06-03 11:29:26 -0700

    patches

--- plotting.py
+++ plotting.py
@@ -14,6 +14,9 @@ import cartopy.crs as ccrs
 import cartopy.io.img_tiles as cimgt
 from read_model import make_gridobj
 from pyproj import Proj
+from shapely.geometry.polygon import Polygon
+from matplotlib.collections import PatchCollection
+from descartes import PolygonPatch
 
 #not sure if a meshgrid or just a 1d list of coordinates is better
 lon = np.arange(-127, -120, 0.5)
@@ -27,24 +30,78 @@ myProj = Proj("+proj=utm +zone=10T, +north +ellps=WGS84 +datum=WGS84 +units=m +n
 UTMx, UTMy = myProj(lonmesh, latmesh)
 g = make_gridobj(lonmesh,latmesh,UTMx, UTMy, ux,uy,uz)
 
-def plot_deformation_map(lon, lat, ux,uy,uz):#and maybe arrow points of decimate later and plot
+def plot_deformation_map(lon, lat, uxg,uyg,uzg,title):#and maybe arrow points of decimate later and plot
     #map of all of cascadia
     longrid = lon.flatten()
     latgrid = lat.flatten()
-    
-    
+    ux = uxg.flatten()
+    uy = uyg.flatten()
+    uz = uzg.flatten()
+
     stamen_terrain = cimgt.StamenTerrain()#desired_tile_form="RGB")
 #    stamen_terrain = cimgt.GoogleTiles(desired_tile_form='RGB', style='satellite')
-    fig = plt.figure(figsize = (8,10))#6,10
+    fig = plt.figure(figsize = (8,8))#6,10
     ax = plt.axes(projection=ccrs.PlateCarree())
     #ax.set_extent([-130.01, -117.99, 40.0, 50.1])
     ax.set_extent([-128.01, -117.99, 40.0, 50.1])
     
-    cmap = mpl.cm.get_cmap('viridis')
+    cmap = mpl.cm.get_cmap('viridis_r')
     normalize = mpl.colors.Normalize(vmin=min(uz), vmax=max(uz))
 #    normalize = mpl.colors.Normalize(vmin=-0.01, vmax=0.01)
 #
     colors = [cmap(normalize(value)) for value in uz]
+    s_m = mpl.cm.ScalarMappable(cmap = cmap, norm=normalize)
+    s_m.set_array([])
+
+    plt.suptitle(title, fontsize = 20)
+
+    ax.add_image(stamen_terrain, 7, alpha = 0.5)#, cmap = 'gray')
+    plt.tick_params(axis='both', which='major', labelsize=18)
+    plt.tick_params(axis='both', which='both', length = 5, width = 1)
+    
+    xticks = [-130, -128, -126, -124, -122, -120, -118]
+    yticks = [40, 42, 44, 46, 48, 50]
+    ax.gridlines(xlocs=xticks, ylocs=yticks, draw_labels = True)
+    plt.scatter(longrid,latgrid, s= 30, c = colors, edgecolors = colors, transform=ccrs.PlateCarree(),alpha = 0.8,label = 'obs points')
+    
+    X, Y = np.meshgrid(lon[0],lat[:,0])
+    U = uxg.flatten()
+    V = uyg.flatten()
+    q = plt.quiver(X, Y, U, V, zorder = 1000,transform=ccrs.PlateCarree())
+    plt.quiverkey(q, X=0.3, Y=1.1, U=1, label='Quiver key', labelpos='E')
+
+    
+    
+    fig.subplots_adjust(right=0.7)
+    cbar_ax = fig.add_axes([0.75, 0.22, 0.02, 0.55])
+    cbar = plt.colorbar(s_m, cax=cbar_ax)
+    cbar.set_label(ur"deformation (m)", fontsize = 20)#"$azimuth$ (\u00b0)"
+    cbar.ax.tick_params(labelsize = 14)
+
+
+    plt.show()
+
+def plot_patches(lat,lon, sslen, dslen,c, label):#and maybe arrow points of decimate later and plot
+    #map of all of cascadia
+    utmxc, utmyc = myProj(lon,lat)
+    print utmxc
+
+    lon1, lat1 = myProj(utmxc+sslen/2,utmyc+dslen/2 , inverse=True)
+    lon0, lat0 = myProj(utmxc-sslen/2,utmyc-dslen/2, inverse=True)
+    
+    vertex_list = [[(lon0[i],lat0[i]), (lon0[i],lat1[i]), (lon1[i],lat1[i]), (lon1[i],lat0[i]), (lon0[i],lat0[i])] for i in range(len(lon1))]
+    print vertex_list
+    
+    stamen_terrain = cimgt.StamenTerrain()#desired_tile_form="RGB")
+    fig = plt.figure(figsize = (8,10))#6,10
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_extent([-128.01, -117.99, 40.0, 50.1])
+    
+    cmap = mpl.cm.get_cmap('viridis')
+    normalize = mpl.colors.Normalize(vmin=min(c), vmax=max(c))
+#    normalize = mpl.colors.Normalize(vmin=-0.01, vmax=0.01)
+#
+    colors = [cmap(normalize(value)) for value in c]
     s_m = mpl.cm.ScalarMappable(cmap = cmap, norm=normalize)
     s_m.set_array([])
 
@@ -54,36 +111,30 @@ def plot_deformation_map(lon, lat, ux,uy,uz):#and maybe arrow points of decimate
     plt.tick_params(axis='both', which='major', labelsize=18)
     plt.tick_params(axis='both', which='both', length = 5, width = 1)
     
-#
-#    horiz=(ux**2+uy**2)**0.5
-#    plt.quiver(longrid,latgrid,ux,uy,color='k')
-#
+    patches = []
+    for i in range(len(vertex_list)):
+        poly = Polygon(vertex_list[i])
+        patches.append(PolygonPatch(poly))
     
     xticks = [-130, -128, -126, -124, -122, -120, -118]
     yticks = [40, 42, 44, 46, 48, 50]
     ax.gridlines(xlocs=xticks, ylocs=yticks, draw_labels = True)
-    plt.scatter(longrid,latgrid, s= 30, c = colors, edgecolors = None, transform=ccrs.PlateCarree(),alpha = 0.7,label = 'obs points')
     
-    X, Y = np.meshgrid(lon[0],lat[:,0])
-    U = ux
-    V = uy
-    q = plt.quiver(X,Y, U, V, zorder = 1000,transform=ccrs.PlateCarree())
-    plt.quiverkey(q, X=0.3, Y=1.1, U=1, label='Quiver key', labelpos='E')
+    
+    p = PatchCollection(patches, alpha=0.4, facecolor = colors)
+    ax.add_collection(p)
 
-    
-    
+
+
     fig.subplots_adjust(right=0.7)
-    cbar_ax = fig.add_axes([0.75, 0.12, 0.02, 0.75])
+    cbar_ax = fig.add_axes([0.75, 0.22, 0.02, 0.55])
     cbar = plt.colorbar(s_m, cax=cbar_ax)
-    cbar.set_label(ur"deformation (m)", fontsize = 22)#"$azimuth$ (\u00b0)"
+    cbar.set_label(label, fontsize = 22)#"$azimuth$ (\u00b0)"
     cbar.ax.tick_params(labelsize = 14)
 
 
 
-    plt.show()
-
-#plot_deformation_map(g.lon, g.lat, [0],[0])
-    
+    plt.show()    
     
 #def compare_models():
 
--- run_all.py
+++ run_all.py
@@ -16,6 +16,8 @@ from read_model import make_gridobj
 from pyproj import Proj
 from read_model import readfile
 from plotting import plot_deformation_map
+from plotting import plot_patches
+
 
 #not sure if a meshgrid or just a 1d list of coordinates is better
 lon = np.arange(-127, -118, 0.2)
@@ -39,58 +41,65 @@ patches_gaus = readfile('gaus.rupt')
 from pyproj import Geod
 wgs84_geod = Geod(ellps='WGS84')
 for p in range(len(patches_wang.lon)):
-#    get patch atrributes
-#    
-#    print patches_wang.utmx[p]
+
     for j in range(len(g.utmx)):
         for i in range(len(g.utmy[0])):
-#            distx = (g.utmx[j][i]-patches_wang.utmx[p])
-#            disty = (g.utmy[j][i]-patches_wang.utmy[p])
             az12,az21,distx = wgs84_geod.inv(g.lon[j][i],g.lat[j][i],patches_wang.lon[p],g.lat[j][i])
             az12,az21,disty = wgs84_geod.inv(g.lon[j][i],g.lat[j][i],g.lon[j][i],patches_wang.lat[p])
-#            print distx, disty
+            depth = patches_wang.z[p] + np.sqrt((patches_wang.ss_len[p]/2.)**2. + (patches_wang.ds_len[p]/2)**2.)
+            ux,uy,uz = calc_deformation(alpha = 2./3, strike = patches_wang.strike[p], depth = depth, dip = patches_wang.dip[p],  strike_width = [-1*patches_wang.ss_len[p]/2., patches_wang.ss_len[p]/2.], dip_width = [-1*patches_wang.ds_len[p]/2., patches_wang.ds_len[p]/2.], dislocation =[patches_wang.ss_slip[p], patches_wang.ds_slip[p], 0], x = distx,y = disty)
+            #next add us to g.u 
+            g.ux[j][i] += ux
+            g.uy[j][i] += uy
+            g.uz[j][i] += uz
 
-            ux,uy,uz = calc_deformation(alpha = 2./3, strike = patches_wang.strike[p], depth = patches_wang.z[p] + np.sqrt((patches_wang.ss_len[p]/2.)**2. + (patches_wang.ds_len[p]/2)**2.), dip = patches_wang.dip[p],  strike_width = [(patches_wang.utmx[p]-patches_wang.ss_len[p])/2., (patches_wang.utmx[p]+patches_wang.ss_len[p])/2.], dip_width = [(patches_wang.utmx[p]-patches_wang.ds_len[p])/2., (patches_wang.utmx[p]+patches_wang.ds_len[p])/2.], dislocation =[patches_wang.ss_slip[p], patches_wang.ds_slip[p], 0], x = distx,y = disty)
+            
+#call plotting function for map  
+plot_deformation_map(lon= g.lon, lat = g.lat, uxg =  g.ux, uyg = g.uy, uzg = g.uz,title = 'gaussian model')
+plt.savefig('figures/wang_fine.png')
+
+
+lon = np.arange(-127, -118, 0.2)
+lat = np.arange(40, 50, 0.2)
+lonmesh, latmesh = np.meshgrid(lon,lat)
+ux = np.zeros((len(lat), len(lon)))
+uy = np.zeros((len(lat), len(lon)))
+uz = np.zeros((len(lat), len(lon)))
+
+myProj = Proj("+proj=utm +zone=10T, +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+UTMx, UTMy = myProj(lonmesh, latmesh)
+g = make_gridobj(lonmesh,latmesh, UTMx, UTMy, ux ,uy, uz)
+
+for p in range(len(patches_gaus.lon)):
+    for j in range(len(g.utmx)):
+        for i in range(len(g.utmy[0])):
+            az12,az21,distx = wgs84_geod.inv(g.lon[j][i],g.lat[j][i],patches_gaus.lon[p],g.lat[j][i])
+            az12,az21,disty = wgs84_geod.inv(g.lon[j][i],g.lat[j][i],g.lon[j][i],patches_gaus.lat[p])
+            depth = patches_gaus.z[p] + np.sqrt((patches_gaus.ss_len[p]/2.)**2. + (patches_gaus.ds_len[p]/2)**2.)
+
+            ux,uy,uz = calc_deformation(alpha = 2./3, strike = patches_gaus.strike[p], depth = depth, dip = patches_gaus.dip[p],  strike_width = [-1*patches_gaus.ss_len[p]/2., patches_gaus.ss_len[p]/2.], dip_width = [-1*patches_gaus.ds_len[p]/2., patches_gaus.ds_len[p]/2.], dislocation =[patches_gaus.ss_slip[p], patches_gaus.ds_slip[p], 0],x=distx,y=disty) #x = g.utmx[j][i],y = g.utmy[j][i])
             #next add us to g.u 
 #            print ux
             g.ux[j][i] += ux
             g.uy[j][i] += uy
             g.uz[j][i] += uz
 
-            
-#call plotting function for map
-        
-plot_deformation_map(lon= g.lon, lat = g.lat, ux =  g.ux.flatten(), uy = g.uy.flatten(), uz = g.uz.flatten())
-plt.savefig('wang_127.png')
+print g.ux
 
-#
-#myProj = Proj("+proj=utm +zone=10T, +north +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
-#UTMx, UTMy = myProj(lonmesh, latmesh)
-#g = make_gridobj(lonmesh,latmesh, UTMx, UTMy, ux ,uy, uz)
-#
-#for p in range(len(patches_gaus.lon)):
-##    get patch atrributes
-##    
-##    print patches_wang.utmx[p]
-#    for j in range(len(g.utmx)):
-#        for i in range(len(g.utmy[0])):
-#            distx = np.abs((g.utmx[j][i]-patches_gaus.utmx[p]))
-#            disty = np.abs((g.utmy[j][i]-patches_gaus.utmy[p]))
-##            patches_wang.utmx[p]
-##            patches_wang.utmx[p]
-##            g.utmx
-##            g.utmy
-##            
-#            ux,uy,uz = calc_deformation(alpha = 2./3, strike = patches_gaus.strike[p], depth = patches_gaus.z[p], dip = patches_gaus.dip[p],  strike_width = [(patches_gaus.utmx[p]-patches_gaus.ss_len[p])/2., (patches_gaus.utmx[p]+patches_gaus.ss_len[p])/2.], dip_width = [(patches_gaus.utmx[p]-patches_gaus.ds_len[p])/2., (patches_gaus.utmx[p]+patches_gaus.ds_len[p])/2.], dislocation =[patches_gaus.ss_slip[p], patches_gaus.ds_slip[p], 0],x=distx,y=disty) #x = g.utmx[j][i],y = g.utmy[j][i])
-#            #next add us to g.u 
-##            print ux
-#            g.ux[j][i] += ux
-#            g.uy[j][i] += uy
-#            g.uz[j][i] += uz
-#
-#print g.ux
-#
-#plot_deformation_map(lon= g.lon, lat = g.lat, ux =  g.ux.flatten(), uy = g.uy.flatten(), uz = g.uz.flatten())
-#plt.savefig('gaus.png')
-#
-##plot_deformation_map(longrid = g.lon.flatten(), latgrid = g.lat.flatten(), uhorizontal = 0, uvertical = 0, deformation = g.uz.flatten())
+plot_deformation_map(lon= g.lon, lat = g.lat, uxg =  g.ux, uyg = g.uy, uzg = g.uz,title = 'gaussian model')
+plt.savefig('figures/gaus_fine.png')
+
+#plot_deformation_map(longrid = g.lon.flatten(), latgrid = g.lat.flatten(), uhorizontal = 0, uvertical = 0, deformation = g.uz.flatten())
+
+plot_patches(patches_gaus.lat, patches_gaus.lon, patches_gaus.ss_len, patches_gaus.ds_len,patches_gaus.ss_slip, label = 'strikeslip')
+plt.savefig('figures/strikeslip.png')
+
+plot_patches(patches_gaus.lat, patches_gaus.lon, patches_gaus.ss_len, patches_gaus.ds_len,patches_gaus.ds_slip, label = 'dipslip')
+plt.savefig('figures/dipslip.png')
+
+
+
+
+
+
+
